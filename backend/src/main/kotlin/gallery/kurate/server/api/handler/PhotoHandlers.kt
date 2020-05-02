a6d91c6be94ad43a30ca04417f08e8fff2a27d89
Jux <justin.boehm@hotmail.de>
2020-05-02 19:19:19 +0200

    Update backend/src/main/kotlin/gallery/kurate/server/api/handler/PhotoHandlers.kt
    Co-authored-by: Selim Dincer <wowselim@live.de>

--- backend/src/main/kotlin/gallery/kurate/server/api/handler/PhotoHandlers.kt
+++ backend/src/main/kotlin/gallery/kurate/server/api/handler/PhotoHandlers.kt
@@ -1,25 +1,18 @@
 package gallery.kurate.server.api.handler
 
 import co.selim.gimbap.api.StreamingStore
+import co.selim.gimbap.api.StreamingStore
 import gallery.kurate.server.api.NotFoundException
-import gallery.kurate.server.api.buildImageUrl
-import gallery.kurate.server.database.AlbumRepository
 import gallery.kurate.server.database.PhotoRepository
 import gallery.kurate.server.ext.dispose
 import gallery.kurate.server.ext.endWithJson
 import gallery.kurate.server.ext.notFound
-import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.internal.operators.observable.ObservableError
 import io.reactivex.schedulers.Schedulers
 import io.vertx.core.Handler
-import io.vertx.core.json.JsonObject
-import io.vertx.kotlin.core.json.jsonArrayOf
 import io.vertx.reactivex.core.buffer.Buffer
 import io.vertx.reactivex.ext.web.RoutingContext
-import java.util.*
-import java.util.logging.Logger
 
 data class ImageHolder(val imageBytes: ByteArray? = null)
 
